10541e1a31d9ace49454312dc6e42d2c9e3a5f4d
safeer2413 <sstnadayara@gmail.com>
2025-09-28 04:33:47 +0530

    deleted unwanted files

--- MernTodo/Backend/MernEcommers/Backend/controllers/adminControler.js
+++
@@ -1,9 +0,0 @@
-import Users from "../Model/userModel.js";
-import asyncHandler from "../middleWare/asyncHandler.js";
-
-
-const getAllUsers = asyncHandler(async(req, res) =>{
-    const users = await Users.find({isAdmin: false});
-
-    req.json(users);
-});
