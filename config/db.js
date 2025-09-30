39df6efcfbd3d7fce6e111f8af223b1a0eeebc24
safeer2413 <sstnadayara@gmail.com>
2025-10-01 04:09:31 +0530

    trying backend connection

--- config/db.js
+++ config/db.js
@@ -11,45 +11,4 @@ const connectDb = async()=>{
     }
 }
 
-
-
 export default connectDb
-
-
-
-// import mongoose from 'mongoose'
-// // import dotenv from 'dotenv'
-// // dotenv.config()
-
-// const connectDb = async () => {
-//     try {
-//         const connect = await mongoose.connect(process.env.MONGO_URL)
-//         console.log('MongoDB Connected Successfully')
-//     } catch (error) {
-//         console.log(error) 
-//     }
-// }
-// //mongodb+srv://backendtodo:sample@backendtodo.bsv33.mongodb.net/?retryWrites=true&w=majority&appName=BackendTodo
-// //backendtodo:sample@backendtodo.bsv33.mongodb.net/?retryWrites=true&w=majority&appName=BackendTodo
-// export default connectDb
-
-
-
-// // import mongoose from "mongoose";
-// // import dotenv from 'dotenv'
-// // dotenv.config()
-
-// // const connectDb = async () => {
-// //     try {
-// //         const connect = await mongoose.connect(process.env.MONGO_URL);
-        
-// //         console.log('MongoDB Connected Successfully')
-// //         // console.log("MONGO_URI:", process.env.MONGO_URL);
-
-// //     } catch (error) {
-// //         console.log(error)  
-// //     }
-// // }
-// // //mongodb+srv://backendtodo:sample@backendtodo.bsv33.mongodb.net/?retryWrites=true&w=majority&appName=BackendTodo
-
-// // export default connectDb
